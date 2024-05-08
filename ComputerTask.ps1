c05fd65b4f2869884ac584d5c8bcf1225793986e
Marcello Lopes <mlopes@wilmorite.com>
2024-05-08 10:15:15 -0400

    Refactor log file path and cleanup in ComputerTask.ps1

--- ComputerTask.ps1
+++ ComputerTask.ps1
@@ -18,6 +18,13 @@ if ($env:COMPUTERNAME -ne "NOAD01" -and $env:COMPUTERNAME -ne "AUTO-J9NH624") {
     return 1
 }
 
+$wingetexe = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
+    if ($wingetexe){
+           $SystemContext = $wingetexe[-1].Path
+    }
+#create the sysget alias so winget can be ran as system
+new-alias -Name sysget -Value "$systemcontext"
+
 ####################################################################################################
 # Install Microsoft Teams
 ####################################################################################################
@@ -41,9 +48,9 @@ $registryPath = "HKLM:\Software\Wilmorite\DCIM"
 $registryItem = "WingetUpdate"
 
 if (-not (Test-Path -Path $registryPath) -or ((Get-Item -LiteralPath $registryPath).GetValue($registryItem, "00000000")) -le (Get-Date).ToString('yyyyMMdd')) {
-    winget upgrade --all --accept-package-agreements --accept-source-agreements
+    sysget upgrade --all --accept-package-agreements --accept-source-agreements
 
-    if (-not (Test-Path -Path $registryPath)) {
+    if ($? -and (-not (Test-Path -Path $registryPath))) {
         New-Item -Path $registryPath -Force | Out-Null
     }
     Set-ItemProperty -Path $registryPath -Name $registryItem -Type String -Value ((get-date).AddDays(1)).ToString('yyyyMMdd') -ErrorAction SilentlyContinue | Out-Null
